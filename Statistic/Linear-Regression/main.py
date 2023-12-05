e8794875bbf336837a498a3a5b0ed6176a517154
İbo Bey <117900246+ibobey@users.noreply.github.com>
2023-12-05 09:42:14 +0300

    Linreg Basic

--- Statistic/Linear-Regression/main.py
+++ Statistic/Linear-Regression/main.py
@@ -1,1 +1,13 @@
-print("Test")
+from numpy import ndarray
+import numpy as np
+from linreg.LinReg import LinReg
+
+
+x: ndarray = np.array([7,4,6,2,1,1,3])
+y: ndarray = np.array([2,4,2,5,7,6,5])
+
+test = LinReg(x=x, y =y)
+
+test.calculate_model()
+
+test.show_model()
